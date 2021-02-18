125d32fc3ee406b0da867e3cf4631f329588fdfa
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-02-18 22:01:32 +0530

    script for solving recurrence relation

---
+++ recurrence.m
@@ -0,0 +1,14 @@
+#!/usr/bin/env octave
+% File: recurrence.m
+% Name: D.Saravanan
+% Solving recurrence relation: a(n+1) = 10 a(n) - 9 a(n-1)
+% a(1) = 2.95, a(2) = 2.95
+
+N = 30
+a = zeros(N,1)
+a(1) = 2.6
+a(2) = 2.6
+
+for n = 2:N-1
+    a(n+1) = 10*a(n) - 9*a(n-1)
+end
